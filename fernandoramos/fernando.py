30fafc96d240735b9c34c34f2e9ada350feb91f9
Fernandoramos24 <87905334+Fernandoramos24@users.noreply.github.com>
2021-08-21 10:13:12 -0600

    primerapracticapython

---
+++ fernandoramos/fernando.py
@@ -0,0 +1,11 @@
+print("se levanta usted a las 7 am")
+
+X = "si"
+
+if X=="si":
+    print("llegaras a tiempo a tu destino")
+
+else:
+    print("es posible que llegues tarde a tu destino")
+
+"Fin Si" 
